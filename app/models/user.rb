53ccb257f44d6525fa9cef9230493feb3a4333b2
codelearner777 <mulyantobudi26@yahoo.com>
2015-11-19 12:45:24 +0700

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,3 +9,4 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: { minimum: 6 }
 
 end
+
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,29 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
+  
   test "should get new" do
     get :new
     assert_response :success
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+      flash[:success] = "Welcome to the Sample App!"
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+
+
 
 end
