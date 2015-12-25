8309d961ff1b1a03ed65508595fcf329a55cce54
codelearner777 <mulyantobudi26@yahoo.com>
2015-12-25 20:22:41 +0700

    Add figaro.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,11 +14,23 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :test
+  #config.action_mailer.raise_delivery_errors = true
+  #config.action_mailer.delivery_method = :test
   host = 'localhost:3000'
-  config.action_mailer.default_url_options = { host: host }
 
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.gmail.com',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => 'protosender@gmail.com',
+    :password       => '123qweiop890',
+    :domain         => 'mail.google.com',
+    :enable_starttls_auto => true
+}
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
