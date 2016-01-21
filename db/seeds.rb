0b00090c70ab778c55ac66e853e92a5f229ca9c1
codelearner777 <mulyantobudi26@yahoo.com>
2016-01-21 21:11:05 +0700

    Add user microposts cloudinary

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,7 +25,7 @@ User.create!(name:  "Example User",
                activated: true,
                activated_at: Time.zone.now )
 end
-  users = User.order(:created-at).take(6)
+  users = User.order(:created_at).take(6)
   50.times do
     content = Faker::Lorem.sentence(5)
     users.each { |user| user.microposts.create!(content: content) }
