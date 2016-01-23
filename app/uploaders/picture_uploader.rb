0184e9297b4cb0be003c7bcf509f5e7b9869b511
codelearner777 <mulyantobudi26@yahoo.com>
2016-01-23 22:14:43 +0700

    cloudinary works on localhost

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -7,25 +7,25 @@ class PictureUploader < CarrierWave::Uploader::Base
   
    include CarrierWave::MiniMagick
    process resize_to_limit: [400, 400]
-   #include Cloudinary::CarrierWave
+   include Cloudinary::CarrierWave
 
   # Choose what kind of storage to use for this uploader:
-  if Rails.env.production?
+  #if Rails.env.production?
     #storage :cloudinary
-     storage :fog
-  else
-    storage :file
+  #   storage :fog
+  #else
+  #  storage :file
   #storage :cloudinary
-  end
+  #end
 
 
 
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
+  #def store_dir
+  #  "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  #end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,10 +2,10 @@ if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
       # Configuration for Amazon S3
-      :provider              => 'AWS',
-      :aws_access_key_id     => ENV['AKIAIBYBILOJ6WRVIYYQ'],
-      :aws_secret_access_key => ENV['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']
+    #  :provider              => 'AWS',
+    #  :aws_access_key_id     => ENV['AKIAIBYBILOJ6WRVIYYQ'],
+    #  :aws_secret_access_key => ENV['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']
     }
-    config.fog_directory     =  ENV['S3_BUCKET']
+    #config.fog_directory     =  ENV['S3_BUCKET']
   end
 end
